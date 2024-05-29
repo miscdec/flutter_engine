4d44ae8d16f5455584117241d2949b0e178b3a79
huangxiaoyao <976125628@qq.com>
2024-05-29 10:06:41 +0800

    解决api12视频播放花屏问题
    Signed-off-by: huangxiaoyao <976125628@qq.com>

--- shell/platform/ohos/ohos_external_texture_gl.cpp
+++ shell/platform/ohos/ohos_external_texture_gl.cpp
@@ -119,8 +119,8 @@ void OHOSExternalTextureGL::Paint(PaintContext& context,
     // The incoming texture is vertically flipped, so we flip it
     // back. OpenGL's coordinate system has Positive Y equivalent to up, while
     // Skia's coordinate system has Negative Y equvalent to up.
-    context.canvas->translate(bounds.x(), bounds.y());
-    context.canvas->scale(bounds.width(), bounds.height());
+    context.canvas->translate(bounds.x(), bounds.y() + bounds.height());
+    context.canvas->scale(bounds.width(), -bounds.height());
 
     if (!transform.isIdentity()) {
       sk_sp<SkShader> shader = image->makeShader(
@@ -148,6 +148,7 @@ void OHOSExternalTextureGL::OnGrContextDestroyed()
 {
   FML_DLOG(INFO)<<" OHOSExternalTextureGL::OnGrContextDestroyed";
   if (state_ == AttachmentState::attached && pixelMap_ == nullptr) {
+    OH_NativeImage_UnsetOnFrameAvailableListener(nativeImage_);
     Detach();
     glDeleteTextures(1, &texture_name_);
     OH_NativeImage_Destroy(&nativeImage_);
@@ -186,9 +187,9 @@ void OHOSExternalTextureGL::Detach()
 void OHOSExternalTextureGL::UpdateTransform()
 {
   float m[16] = { 0.0f };
-  int32_t ret = OH_NativeImage_GetTransformMatrix(nativeImage_, m);
+  int32_t ret = OH_NativeImage_GetTransformMatrixV2(nativeImage_, m);
   if (ret != 0) {
-    FML_DLOG(FATAL)<<"OHOSExternalTextureGL OH_NativeImage_GetTransformMatrix err code:"<< ret;
+    FML_DLOG(FATAL)<<"OHOSExternalTextureGL OH_NativeImage_GetTransformMatrixV2 err code:"<< ret;
   }
   // transform ohos 4x4 matrix to skia 3x3 matrix
   SkScalar matrix3[] = {
