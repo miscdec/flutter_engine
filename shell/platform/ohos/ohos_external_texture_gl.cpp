bea6da4b9ad610930efad967389f9b6c5798a93a
18719058668 <718092089@qq.com>
2024-06-17 09:50:54 +0800

    解决webview二次进入白色背景色失效问题
    Signed-off-by: 18719058668 <718092089@qq.com>

--- shell/platform/ohos/ohos_external_texture_gl.cpp
+++ shell/platform/ohos/ohos_external_texture_gl.cpp
@@ -102,21 +102,17 @@ void OHOSExternalTextureGL::Paint(PaintContext& context,
   }
   if (state_ == AttachmentState::uninitialized) {
     Attach();
-  }
-
-  if (!freeze) {
-    if (!first_update_) {
-      if (!isEmulator_) {
-        setBackground(bounds.width(), bounds.height());
-      }
-      Update();
-      first_update_ = true;
-    } else if (new_frame_ready_ && pixelMap_ != nullptr) {
+    if (!freeze && new_frame_ready_ && pixelMap_ != nullptr) {
       ProducePixelMapToNativeImage();
       Update();
-      new_frame_ready_ = false;
-    }
+	}
+    new_frame_ready_ = false;
+  }
 
+  if (!freeze && !first_update_ && !isEmulator_ && !new_frame_ready_ && pixelMap_ == nullptr) {
+    setBackground(bounds.width(), bounds.height());
+    Update();
+    first_update_ = true;
   }
 
   GrGLTextureInfo textureInfo = {
@@ -176,13 +172,13 @@ void OHOSExternalTextureGL::MarkNewFrameAvailable()
 {
   FML_DLOG(INFO)<<" OHOSExternalTextureGL::MarkNewFrameAvailable";
   new_frame_ready_ = true;
-  first_update_ = true;
   Update();
 }
 
 void OHOSExternalTextureGL::OnTextureUnregistered()
 {
   FML_DLOG(INFO)<<" OHOSExternalTextureGL::OnTextureUnregistered";
+  first_update_ = false;
   OH_NativeImage_UnsetOnFrameAvailableListener(nativeImage_);
   OH_NativeImage_Destroy(&nativeImage_);
 }
@@ -194,6 +190,7 @@ void OHOSExternalTextureGL::Update()
     FML_DLOG(FATAL)<<"OHOSExternalTextureGL OH_NativeImage_UpdateSurfaceImage err code:"<< ret;
     return;
   }
+  first_update_ = true;
   UpdateTransform();
 }
 
