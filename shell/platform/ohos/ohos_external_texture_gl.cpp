4e7a15171ed0859a51e24c0c876adc702bf519be
wwyang <137208408@qq.com>
2024-07-08 15:25:49 +0800

    解决大尺寸图片crash问题
    Signed-off-by: wwyang <137208408@qq.com>

--- shell/platform/ohos/ohos_external_texture_gl.cpp
+++ shell/platform/ohos/ohos_external_texture_gl.cpp
@@ -374,6 +374,11 @@ void OHOSExternalTextureGL::ProducePixelMapToNativeImage()
     FML_DLOG(ERROR) << "OHOSExternalTextureGL OH_NativeWindow_NativeWindowHandleOpt err:" << ret;
   }
 
+  int32_t usage = 0;
+  OH_NativeWindow_NativeWindowHandleOpt(nativeWindow_, GET_USAGE, &usage);
+  usage |= NATIVEBUFFER_USAGE_CPU_READ;
+  OH_NativeWindow_NativeWindowHandleOpt(nativeWindow_, SET_USAGE, usage);
+
   if (buffer_ != nullptr) {
     OH_NativeWindow_NativeWindowAbortBuffer(nativeWindow_, buffer_);
     buffer_ = nullptr;
